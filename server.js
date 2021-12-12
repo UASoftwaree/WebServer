dbe9c950df3c40bab35f65296adcc0555d7e0770
Ulric <57965846+daulric@users.noreply.github.com>
2021-12-12 19:55:55 -0400

    just made a webserver

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const app = express();
 
+const publicPath = "/public"
+
+//404 Webpage
+
+//Webserver Redirect
+app.get('/sponsor',(req,res)=>res.redirect("https://daulric.tk"))
+
+
+//Accessing Files without the use of extentions
+app.get('',(req,res)=>res.sendFile(__dirname + publicPath + "/index.html"))
+app.get('/hello',(req,res)=>res.sendFile(__dirname + publicPath + "/hello.html"))
+
+//404 Webpage
+app.use(express.static(__dirname + "/public"), (_, res, next) => {
+  res.status(404)
+  res.sendFile(__dirname + publicPath + "/404.html")
+});
+
+app.listen(8080);
